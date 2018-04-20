76c2039701f071d3cb94069b4eb19d792af2051b
coding dog <sayyes566@gmail.com>
2018-04-20 11:05:40 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,12 @@
-<div id="UHSearchBox">
-  <?php echo("HELLO PHP"); ?>
-</div>
 
+<html>
   <head>
     <title>Hello TaaS!</title>
   </head>
+<body>
+<h1>CI test </h1>
+<div id="UHSearchBox">
+  <?php echo("PHP"); ?>
+</div>
+</body>
+</html>
