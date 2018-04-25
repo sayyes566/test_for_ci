e141ae77d0d9f6a933b75e125a97e0fcb85c06ee
coding dog <sayyes566@gmail.com>
2018-04-25 14:59:55 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,13 +5,13 @@ import sys, argparse
 
 class SearchText(unittest.TestCase):
    def setUp(self):
-        self.test_ip=args.input
-        print( self.test_ip)
+        self.test_url="http://35.201.172.208/test.php"
+        print( self.test_url)
         # create a new session
         self.chrome = webdriver.Remote(
             command_executor='http://10.55.242.84:4444/wd/hub',
             desired_capabilities=DesiredCapabilities.CHROME)
-        self.chrome.get('http://' + self.test_ip)
+        self.chrome.get(' + self.test_url + ')
    def test_search_by_text(self):
         #lists = self.chrome.find_elements_by_class_name("w10")
         #no=len(lists)
@@ -22,7 +22,8 @@ class SearchText(unittest.TestCase):
         self.chrome.quit()
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', default='My Input')
+    parser.add_argument('--driverip', default='My web driver ip')
+    parser.add_argument('--testurl', default='My test url')
     parser.add_argument('unittest_args', nargs='*')
     args = parser.parse_args()
     sys.argv[1:] = args.unittest_args
